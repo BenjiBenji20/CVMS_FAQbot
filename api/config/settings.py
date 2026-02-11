9378f972ae540160e90e1e47052c20a6fab29c11
BenjiBenji20 <imperialbenjie393@gmai.com>
2026-02-11 21:03:29 +0800

    feat: redis uri conf

--- api/config/settings.py
+++ api/config/settings.py
@@ -14,6 +14,17 @@ class Settings(BaseSettings):
     
     REQUEST_SECRET_KEY: str | None = None
     
+    UPSTASH_REDIS_REST_URL: str | None = None
+    UPSTASH_REDIS_REST_TOKEN: str | None = None
+    UPSTASH_REDIS_PORT: int | None = 6379 # default redis port
+    
+    # redis conf
+    def get_redis_client_uri(self) -> str:
+        return (
+            f"rediss://default:{self.UPSTASH_REDIS_REST_TOKEN}"
+            f"@{self.UPSTASH_REDIS_REST_URL}:{self.UPSTASH_REDIS_PORT}"
+        )
+    
     class Config:
         env_file = ".env"
         extra = "ignore"
